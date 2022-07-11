a23255ae42e273769e800a90a3ae962f4006ea85
souravdasdip <97696849+souravdasdip@users.noreply.github.com>
2022-07-12 00:38:05 +0600

    User sign in successful

--- client/src/screens/Login.js
+++ client/src/screens/Login.js
@@ -1,13 +1,27 @@
+import { useMutation } from "@apollo/client";
 import React from "react";
 import FormInput from "../components/FormInput";
+import { SIGNIN_USER } from "../services/mutation";
 
 function Login() {
+  const [signInUser, { data, loading, error }] = useMutation(SIGNIN_USER);
+
   const handleLogin = (e) => {
     e.preventDefault();
     const formdata = new FormData(e.target);
     const data = Object.fromEntries(formdata.entries());
     console.log(data);
+    //sign in to server
+    signInUser({
+      variables: {
+        userSignin: data,
+      },
+    });
   };
+
+  if (data) {
+    localStorage.setItem("token", data.user.token);
+  }
   return (
     <div
       style={{
@@ -23,6 +37,7 @@ function Login() {
         <FormInput name="email" placeholder="Enter Email..." />
         <FormInput name="password" placeholder="Enter Password..." />
         <button>Login</button>
+        {data && <p>{data.user.token}</p>}
       </form>
     </div>
   );
--- client/src/services/mutation.js
+++ client/src/services/mutation.js
@@ -7,3 +7,11 @@ export const SIGNUP_USER = gql`
     }
   }
 `;
+
+export const SIGNIN_USER = gql`
+  mutation SigninUser($userSignin: UserSignInput!) {
+    user: signinUser(userSignin: $userSignin) {
+      token
+    }
+  }
+`;
