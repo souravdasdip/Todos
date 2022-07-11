7713f2ddf05a83bcff2ec7cd7adfe51189a3eab8
souravdasdip <97696849+souravdasdip@users.noreply.github.com>
2022-07-11 23:30:24 +0600

    Testing with fetch method in client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
+import { useEffect } from "react";
 import "./App.css";
 import Login from "./screens/Login";
 import Registration from "./screens/Registration";
 
 function App() {
+  useEffect(() => {
+    fetch("http://localhost:4000/", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+          query getAllQuotes{
+            quotes{
+              name
+            }
+          }
+        `,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="App">
       <Registration />
