cc193b3040e2bf52cf46404b2594bf8597e13d49
souravdasdip <97696849+souravdasdip@users.noreply.github.com>
2022-07-12 00:22:52 +0600

    User signup successful

--- client/src/screens/Registration.js
+++ client/src/screens/Registration.js
@@ -1,14 +1,25 @@
+import { useMutation } from "@apollo/client";
 import React from "react";
 import FormInput from "../components/FormInput";
+import { SIGNUP_USER } from "../services/mutation";
 
 function Registration() {
+  const [signupUser, { data, loading, error }] = useMutation(SIGNUP_USER);
   const handleRegistration = (e) => {
     e.preventDefault();
     const formdata = new FormData(e.target);
     const data = Object.fromEntries(formdata.entries());
     console.log(data);
-    formdata.delete("firstName");
+    signupUser({
+      variables: {
+        userNew: data,
+      },
+    });
   };
+
+  console.log({ loading });
+  console.log({ data });
+  console.log({ error });
   return (
     <div
       style={{
@@ -26,6 +37,7 @@ function Registration() {
         <FormInput name="email" placeholder="Enter Email..." />
         <FormInput name="password" placeholder="Enter Password..." />
         <button>Registration</button>
+        {data && <h6>You can signin "{data.user.firstName}"</h6>}
       </form>
     </div>
   );
